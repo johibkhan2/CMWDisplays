64ce2036f344a177eb7a32683a1b47b149a1aa77
florian <floriandzemov@gmail.com>
2017-10-17 11:05:48 +0530

    header

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,9 +1,25 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Header.css'
 
 class Header extends Component {
   render() {
-    return (<div></div>);
+    return (
+      <div>
+        <div className="container main-header">
+          <div className="row mn-hd">
+          <div className="col-md-12 col-md-offset-2">
+          <header className="head">
+            <button className="cmw-btn"><i className="btn-hm-icn fa fa-home"></i><span className="btn-text"> Home </span></button>
+
+            <button className="cmw-btn"><i className="btn-dn-icn fa fa-download"></i><span className="btn-text"> Download Display Controller </span></button>
+
+            <button className="cmw-btn"><i className="btn-ex-icn fa fa-sign-out"></i><span className="btn-text"> Exit </span></button>
+          </header>
+          </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
