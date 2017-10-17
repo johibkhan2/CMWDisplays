1187018a7fbf4f6407688203ac5563282e9212cb
johibkhan2 <johib.khan@gmail.com>
2017-10-17 15:26:12 +0530

    adding login component

---
+++ src/shared/Login.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+
+
+class Login extends Component{
+
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+
+
+  render () {
+
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+
+
+export default Login;
