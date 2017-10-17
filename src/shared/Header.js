2f362e595be0ebfc41db665438da8407c82498c4
SohibKhan <sohib.khan22@gmail.com>
2017-10-17 14:22:32 +0530

    navbar

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -7,15 +7,15 @@ class Header extends Component {
       <div>
         <div className="container main-header">
           <div className="row mn-hd">
-          <div className="col-md-12 col-md-offset-2">
-          <header className="head">
-            <button className="cmw-btn"><i className="btn-hm-icn fa fa-home"></i><span className="btn-text"> Home </span></button>
-
-            <button className="cmw-btn"><i className="btn-dn-icn fa fa-download"></i><span className="btn-text"> Download Display Controller </span></button>
-
-            <button className="cmw-btn"><i className="btn-ex-icn fa fa-sign-out"></i><span className="btn-text"> Exit </span></button>
-          </header>
-          </div>
+            <div className="col-md-12 col-md-offset-2">
+              <header className="head">
+                <nav className="navbar">
+                  <button className="cmw-btn"><i className="btn-hm-icn fa fa-home"></i><span className="btn-text"> Home </span></button>
+                  <button className="cmw-btn"><i className="btn-dn-icn fa fa-download"></i><span className="btn-text"> Download Display Controller </span></button>
+                  <button className="cmw-btn"><i className="btn-ex-icn fa fa-sign-out"></i><span className="btn-text"> Exit </span></button>
+                </nav>
+              </header>
+            </div>
           </div>
         </div>
       </div>
