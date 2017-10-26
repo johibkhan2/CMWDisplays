72c26661dfd8f758ca1a03a0d04200d02e8f2439
johibkhan2 <johib.khan@gmail.com>
2017-10-26 13:14:18 +0530

    password validation done

--- src/auth/ChangePassword.js
+++ src/auth/ChangePassword.js
@@ -5,20 +5,79 @@ import Alert from 'react-s-alert';
 import * as validator from '../validation/validator';
 import  Modal from 'react-modal';
 
-
+        // Blank = 0,
+        // VeryWeak = 1,
+        // Weak = 2,
+        // Medium = 3,
+        // Strong = 4,
+        // VeryStrong = 5
 
 class ChangePassword extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            oldPassword:'',
+            newPassword:'',
+            confirmPassword:''
         };
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+
+    isValid(oldPassword, newPassword, confirmPassword){
+    if (this.isRequired(oldPassword, newPassword,confirmPassword)) {
+        const score = validator.getPasswordScore(newPassword);
+        console.log("score" + score);
+        if (score >= 3) {
+            if (this.isEqualPassword(newPassword, confirmPassword)) {
+                return true;
+            } else {
+                Alert.error('<h4>new and confirm passwords must be equal</h4>');
+                return false;
+            }
+        } else {
+            Alert.error('<h4>Password is weak, Please create password as per hint</h4>');
+            return false;
+        }
+    } else {
+        Alert.error('<h4>All fields are mandatory</h4>');
+        return false;
+    }
+
+
+    }
+
+    isRequired(oldPassword, newPassword, confirmPassword) {
+        if (validator.isRequired(oldPassword) && validator.isRequired(newPassword) && validator.isRequired(confirmPassword)) {
+            return true;
+        }
+        return false;
+    }
+
+    isEqualPassword(newPassword, confirmPassword){
+        if(newPassword==confirmPassword) return true;
+        return false;
     }
 
 
     handleSubmit(event) {
-        this.props.closeModalChgPass();
+        event.preventDefault();
+        if (this.isValid(this.state.oldPassword, this.state.newPassword, this.state.confirmPassword)) {
+            this.props.closeModalChgPass();
+        }
+
+    }
+
+    handleChange(event) {
+        event.preventDefault();
+        const target = event.target;
+        const value = target.value;
+        const id = target.id;
+        this.setState({
+        [id]: value
+        });
     }
 
 
@@ -27,7 +86,7 @@ class ChangePassword extends React.Component {
         
             <div className="lgn ibox-content">
                 
-                <form className="form-horizontal" onSubmit={this.handleSubmit}>
+                <form className="form-horizontal">
 
                     <div className="form-group">
                         <label className="txt-clr col-lg-2 control-label">Old Password:</label>
@@ -35,7 +94,9 @@ class ChangePassword extends React.Component {
                             <input
                                 type="text"
                                 className="form-control"
-                                id="userName"
+                                id="oldPassword"
+                                value={this.state.oldPassword}
+                                onChange={this.handleChange}
                             />
                         </div>
                     </div>
@@ -44,7 +105,9 @@ class ChangePassword extends React.Component {
                         <label className="txt-clr col-lg-2 control-label">New Password:</label>
                         <div className="col-lg-10">
                             <input type="text" className="form-control" 
-                             id="DealerId" 
+                             id="newPassword"
+                             value={this.state.newPassword}
+                             onChange={this.handleChange}
                             />
                         </div>
                     </div>
@@ -53,9 +116,19 @@ class ChangePassword extends React.Component {
                         <label className="txt-clr col-lg-2 control-label">Confirm Password:</label>
                         <div className="col-lg-10">
                             <input type="password" className="form-control"
-                             id="password"
+                             id="confirmPassword"
+                             value={this.state.confirmPassword}
+                             onChange={this.handleChange}
                             />
                         </div>
+                    </div>
+
+                    <div className="row">
+                    <h4>Password should have at least 7 characters and atleast special characters/digit/upper case </h4>
+                    </div>
+
+                    <div id="passwordStrength" className="row">
+                    <h4> </h4>
                     </div>
 
                     <div className="row">
@@ -70,11 +143,10 @@ class ChangePassword extends React.Component {
                     <div className="col-sm-1">
                     </div>
                     <div className="col-sm-2">                 
-                    <button  className=".btn-default"  onClick={this.handleSubmit}>LogIn</button>  
+                    <button  className=".btn-default"  onClick={this.handleSubmit}>Submit</button>  
                     </div> 
                     </div>                   
                     </div>
-
                 </form>
 
             </div>
--- src/validation/validator.js
+++ src/validation/validator.js
@@ -6,3 +6,43 @@ export function isRequired(val) {
         return true;
     }
 }
+
+
+export function getPasswordScore(val) {
+    let score = 1;
+    let value = val.trim();
+    if(value.length >= 7) {
+        score++;
+    } if(value.length >= 12) {
+        score++;
+    } if(atLeastNumber(value)) {
+        score++;
+    } if(atLeastSpecChar(value)) {
+        score++;
+    } if(atLeastUpperChar(value)) {
+        score++;
+    }
+    return score;
+}
+
+
+export function atLeastNumber(val) {
+if(val.match(/\d+/))
+    return true;
+ return false;
+}
+
+export function atLeastSpecChar(val) {
+if(val.match(/.[!,@,#,$,%,^,&,*,?,_,~,-,(,),'\s']/)) 
+    return true;
+ return false;
+}
+
+export function atLeastUpperChar(val) {
+if(val.match(/[A-Z]/) && val.match(/[a-z]/)) 
+    return true;
+ return false;
+}
+
+
+
